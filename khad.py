ded34dbdc7ae122f952c978f1c59dd9eedd51bd4
mohamedvadhel <121568913+mohamedvadhel@users.noreply.github.com>
2023-08-07 14:11:48 +0100

    Update khad.py

--- khad.py
+++ khad.py
@@ -2,19 +2,13 @@ import streamlit as st
 
 def main():
     # Set the page title and description
-    st.set_page_config(page_title='Khadouj El Yedali', page_icon=":heart:")
+    st.set_page_config(page_title='Hello World', page_icon="🌍")
 
-    # Add a personalized message to Khadouj
-    st.title('Hello, Khadouj!')
-    st.write(" 4i interface application s9ira mou3adla lla lk nti")
+    # Add the title to the interface
+    st.title('Hello, World!')
 
-    # Upload the video file
-    uploaded_file = st.file_uploader("Upload your video", type=["mp4", "avi", "mov"])
-
-    # Check if a video file was uploaded
-    if uploaded_file is not None:
-        # Display the video
-        st.video(uploaded_file)
+    # Write a message
+    st.write('Welcome to this simple Streamlit app that says "Hello, World!"')
 
 if __name__ == '__main__':
     main()
